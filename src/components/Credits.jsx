b9727484f45615277addeafab76edbac9d38611a
Make_aguess <94730074+dabby12@users.noreply.github.com>
2024-11-03 21:24:47 +0800

    Update Credits.jsx

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -6,10 +6,10 @@ const Credits = () => {
     <div className="bg-gray-800 text-purple-500 p-8">
       <h2 className="text-2xl font-bold mb-4 hover:text-yellow-400">Credits</h2>
       <ul className="list-disc list-inside space-y-2">
-        <li className="hover:text-green-300">Design: <span className="font-semibold hover:text-green-500">Chris Poh</span></li>
-        <li className="hover:text-indigo-400">Development: <span className="font-semibold hover:text-blue-500">Jane Smith</span></li>
-        <li className="transition duration-300 hover:text-orange-500">Copywriting: <span className="font-semibold">Alice Johnson</span></li>
-        <li>Photography: <span className="font-semibold">Bob Brown</span></li>
+        <li className="hover:text-green-300">Design: <span className="font-semibold hover:text-green-500">Chris Liu</span></li>
+        <li className="hover:text-indigo-400">Development: <span className="font-semibold hover:text-blue-500">Chris Liu</span></li>
+        <li className="transition duration-300 hover:text-orange-500">Copywriting: <span className="font-semibold">Chris Liu/span></li>
+        <li>Idea/ Leaders: <span classname="font-semibold">Ian Tay, Isaac Poh</li>
       </ul>
       <p className="mt-4 ">Thank you to everyone who contributed!</p>
     </div>
