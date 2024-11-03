7140f55068ac486199128fc492be644cca01b458
Make_aguess <94730074+dabby12@users.noreply.github.com>
2024-11-03 21:28:44 +0800

    Update Credits.jsx

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -9,7 +9,7 @@ const Credits = () => {
         <li className="hover:text-green-300">Design: <span className="font-semibold hover:text-green-500">Chris Liu</span></li>
         <li className="hover:text-indigo-400">Development: <span className="font-semibold hover:text-blue-500">Chris Liu</span></li>
         <li className="transition duration-300 hover:text-orange-500">Copywriting: <span className="font-semibold">Chris Liu/span></li>
-        <li>Idea/ Leaders: <span classname="font-semibold">Ian Tay, Isaac Poh</li>
+        
       </ul>
       <p className="mt-4 ">Thank you to everyone who contributed!</p>
     </div>
