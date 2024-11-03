1246936c06f8af53202dd18893427aaa14f1d505
Make_aguess <94730074+dabby12@users.noreply.github.com>
2024-11-03 21:35:18 +0800

    Update Credits.jsx

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,4 +1,3 @@
-// src/Credits.js
 import React from 'react';
 
 const Credits = () => {
@@ -8,10 +7,9 @@ const Credits = () => {
       <ul className="list-disc list-inside space-y-2">
         <li className="hover:text-green-300">Design: <span className="font-semibold hover:text-green-500">Chris Liu</span></li>
         <li className="hover:text-indigo-400">Development: <span className="font-semibold hover:text-blue-500">Chris Liu</span></li>
-        <li className="transition duration-300 hover:text-orange-500">Copywriting: <span className="font-semibold">Chris Liu </span></li>
-        
+        <li className="transition duration-300 hover:text-orange-500">Copywriting: <span className="font-semibold">Chris Liu</span></li>
       </ul>
-      <p className="mt-4 ">Thank you to everyone who contributed!</p>
+      <p className="mt-4">Thank you to everyone who contributed!</p>
     </div>
   );
 };
